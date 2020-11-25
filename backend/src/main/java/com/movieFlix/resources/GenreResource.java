6eb1ab29f95706e56b1c887989fb1d80c06b3aea
RanyellHenrique <ranyellhenrique@gmail.com>
2020-11-25 16:23:31 -0300

    Resource Genre

---
+++ backend/src/main/java/com/movieFlix/resources/GenreResource.java
@@ -0,0 +1,28 @@
+package com.movieFlix.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.movieFlix.dto.GenreDTO;
+import com.movieFlix.services.GenreService;
+
+@RestController
+@RequestMapping(value = "genres")
+public class GenreResource {
+	
+	
+	@Autowired
+	private GenreService service;
+	
+	@GetMapping
+	public ResponseEntity<List<GenreDTO>> findAll(){
+		List<GenreDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+}
---
+++ backend/src/main/java/com/movieFlix/services/GenreService.java
@@ -0,0 +1,24 @@
+package com.movieFlix.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.movieFlix.dto.GenreDTO;
+import com.movieFlix.entities.Genre;
+import com.movieFlix.repositories.GenreRepository;
+
+@Service
+public class GenreService {
+	
+	@Autowired
+	private GenreRepository repository;
+	
+	public List<GenreDTO> findAll(){
+		List<Genre> list = repository.findAll();
+		return list.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());
+	}
+
+}
