c5959794ebc0b2079c5da84946f828253c64deb2
RanyellHenrique <ranyellhenrique@gmail.com>
2020-11-25 00:28:12 -0300

    Entities Movie and Genre

--- backend/src/main/java/com/movieFlix/entities/Review.java
+++ backend/src/main/java/com/movieFlix/entities/Review.java
@@ -24,9 +24,22 @@ public class Review implements Serializable {
 	@JoinColumn(name = "user_id")
 	private User user;
 	
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	
 	public Review() {
 		
 	}
+
+	public Review(Long id, String text, User user, Movie movie) {
+		super();
+		this.id = id;
+		this.text = text;
+		this.user = user;
+		this.movie = movie;
+	}
+
 
 	public Long getId() {
 		return id;
@@ -50,6 +63,14 @@ public class Review implements Serializable {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+	
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
 	}
 
 	@Override
